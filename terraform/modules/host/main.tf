37a22201f335cb5bf6ab3a71915a579d6e2e75af
Seumas Dunlop <seumas.dunlop@coralbay.tv>
2022-08-05 13:32:29 +1200

    Support Extra Bridges, VM Name and CPU Mode
    Add 'cluster.nodes.worker.instances..extra_bridges' setting. Creates
    additional network interfaces in the VM and maps them to a pre-configured
    bridge on the host. A static IP address is added for each to the neplan
    config. Child settings:
      bridge - name of the host bridge. E.g. br1
      network_interface - must match the name of the network link in the VM
        for this network interface. E.g. ens4
      ipCidr - IP address to assign in CIDR format. E.g. 192.168.0.100/24
    
    Add 'cluster.nodes.worker.instances..name' setting. The VM name is formed
    by appending this value to the cluster name. Default is to use the worker id.
    
    Add 'cluster.nodeTemplate.cpuMode' setting. Sets the CPU mode used by the
    VM (e.g. host-passthrough). Default: 'custom'.
    
    Don't define the IP address when generating the libvirt_domain resource.
    The IP is set by netplan config and this generates constant changes in the
    terraform state.
    
    Fix an error when generating the output if no IPs are set on a VM.

--- terraform/modules/host/main.tf
+++ terraform/modules/host/main.tf
@@ -89,6 +89,7 @@ module "lb_module" {
   network_bridge   = var.cluster_network_bridge
   network_gateway  = var.cluster_network_gateway != null ? var.cluster_network_gateway : cidrhost(var.cluster_network_cidr, 1)
   network_cidr     = var.cluster_network_cidr
+  extra_bridges = []
 
   # Load balancer specific variables #
   vm_name              = "${var.cluster_name}-${var.node_types.load_balancer}-${each.value.id}"
@@ -99,6 +100,7 @@ module "lb_module" {
   vm_ssh_known_hosts   = var.cluster_nodeTemplate_ssh_addToKnownHosts
   vm_network_interface = var.cluster_nodeTemplate_os_networkInterface
   vm_dns               = var.cluster_nodeTemplate_dns
+  vm_cpuMode           = var.cluster_nodeTemplate_cpuMode
   vm_cpu               = each.value.cpu != null ? each.value.cpu : var.cluster_nodes_loadBalancer_default_cpu
   vm_ram               = each.value.ram != null ? each.value.ram : var.cluster_nodes_loadBalancer_default_ram
   vm_main_disk_size    = each.value.mainDiskSize != null ? each.value.mainDiskSize : var.cluster_nodes_loadBalancer_default_mainDiskSize
@@ -136,6 +138,7 @@ module "master_module" {
   network_bridge   = var.cluster_network_bridge
   network_gateway  = var.cluster_network_gateway != null ? var.cluster_network_gateway : cidrhost(var.cluster_network_cidr, 1)
   network_cidr     = var.cluster_network_cidr
+  extra_bridges = []
 
   # Master node specific variables #
   vm_name              = "${var.cluster_name}-${var.node_types.master}-${each.value.id}"
@@ -146,6 +149,7 @@ module "master_module" {
   vm_ssh_known_hosts   = var.cluster_nodeTemplate_ssh_addToKnownHosts
   vm_network_interface = var.cluster_nodeTemplate_os_networkInterface
   vm_dns               = var.cluster_nodeTemplate_dns
+  vm_cpuMode           = var.cluster_nodeTemplate_cpuMode
   vm_cpu               = each.value.cpu != null ? each.value.cpu : var.cluster_nodes_master_default_cpu
   vm_ram               = each.value.ram != null ? each.value.ram : var.cluster_nodes_master_default_ram
   vm_main_disk_size    = each.value.mainDiskSize != null ? each.value.mainDiskSize : var.cluster_nodes_master_default_mainDiskSize
@@ -183,9 +187,10 @@ module "worker_module" {
   network_bridge   = var.cluster_network_bridge
   network_gateway  = var.cluster_network_gateway != null ? var.cluster_network_gateway : cidrhost(var.cluster_network_cidr, 1)
   network_cidr     = var.cluster_network_cidr
+  extra_bridges = each.value.extra_bridges
 
   # Worker node specific variables #
-  vm_name              = "${var.cluster_name}-${var.node_types.worker}-${each.value.id}"
+  vm_name              = each.value.name != null ? "${var.cluster_name}-${each.value.name}" : "${var.cluster_name}-${var.node_types.worker}-${each.value.id}"
   vm_type              = var.node_types.worker
   vm_user              = var.cluster_nodeTemplate_user
   vm_update            = var.cluster_nodeTemplate_updateOnBoot
@@ -193,6 +198,7 @@ module "worker_module" {
   vm_ssh_known_hosts   = var.cluster_nodeTemplate_ssh_addToKnownHosts
   vm_network_interface = var.cluster_nodeTemplate_os_networkInterface
   vm_dns               = var.cluster_nodeTemplate_dns
+  vm_cpuMode           = var.cluster_nodeTemplate_cpuMode
   vm_cpu               = each.value.cpu != null ? each.value.cpu : var.cluster_nodes_worker_default_cpu
   vm_ram               = each.value.ram != null ? each.value.ram : var.cluster_nodes_worker_default_ram
   vm_main_disk_size    = each.value.mainDiskSize != null ? each.value.mainDiskSize : var.cluster_nodes_worker_default_mainDiskSize
--- terraform/modules/vm/output.tf
+++ terraform/modules/vm/output.tf
@@ -3,7 +3,7 @@ output "vm_info" {
     id     = var.vm_id
     type   = var.vm_type
     name   = libvirt_domain.vm_domain.name,
-    ip     = libvirt_domain.vm_domain.network_interface.0.addresses.0
+    ip     = try(libvirt_domain.vm_domain.network_interface.0.addresses.0, null)
   }
   description = "VM's info containing it's name and an IP address"
 }
